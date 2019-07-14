f876deee611d3c1f02f9b7ef80b11f285bf44e63
ehtisham91 <39628264+ehtisham91@users.noreply.github.com>
2019-07-14 12:27:22 +0500

    resolve xlsx path error

--- credentials.py
+++ credentials.py
@@ -7,5 +7,3 @@ Password = 'pswd'
 # path of chrome driver
 Driver_path = 'C:\\Users\\ehtis\\Downloads\\chromedriver.exe'
 
-# path of directory where you want to save mobile numbers in xls file
-Path = 'C:\\Users\\Downloads'
